36633c9f1c5bbbea6243f64396b0b22700fd06d5
RainbowIsPerfect <andrey-smollin@rambler.ru>
2023-02-05 21:27:35 +0300

    function done() {     d.removeListener('error', onError);     d.exit();     return tryCatch(cb, arguments);   }

--- gulpfile.js
+++ gulpfile.js
@@ -1,169 +1,58 @@
-const gulp = require('gulp');
-const del = require('del');
-const sass = require('gulp-sass')(require('sass'));
-const cleanCSS = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
-const concat = require('gulp-concat');
-const autoprefix = require('gulp-autoprefixer');
-const imagemin = require('gulp-imagemin');
-const htmlmin = require('gulp-htmlmin');
-const fileinclude = require('gulp-file-include');
-const newer = require('gulp-newer');
-const webp = require('gulp-webp');
-const webpHtml = require('gulp-webp-html-nosvg');
-const htmlpretty = require('gulp-pretty-html');
-const fonter = require('gulp-fonter');
-const woff = require('gulp-ttf2woff');
-const woff2 = require('gulp-ttf2woff2');
-const browserSync = require('browser-sync').create();
-const git = require('gulp-git');
-const zip = require('gulp-zip');
+import gulp from "gulp";
+import { paths } from "./config/paths.js";
+import { plugins } from "./config/plugins.js";
 
+// const git = require('gulp-git');
+// const zip = require('gulp-zip');
 
-
-const paths = {
-    styles: {
-        src: 'src/styles/style.scss',
-        stylewatch: 'src/styles/**/*.scss',
-        dest: 'dist/css/'
-    },
-    scripts: {
-        src: 'src/scripts/**/*.js',
-        dest: 'dist/js/'
-    },
-    images: {
-        src: 'src/img/**',
-        dest: 'dist/img/'
-    },
-    html: {
-        src: 'src/*.html',
-        htmlwatch: 'src/**/*.html',
-        dest: 'dist'
-    },
-    fonts: {
-        src: 'src/fonts/',
-        dest: 'dist/fonts/',
-    }
+global = {
+	gulp: gulp,
+    paths: paths,
+    plugins: plugins,
 }
 
-function clean() {
-    return del(['dist', '!dist/img', '!dist/fonts'])
-}
-
-function html() {
-    return gulp.src(paths.html.src)
-        .pipe(fileinclude())
-        .pipe(webpHtml())
-        // if needed
-        // .pipe(htmlmin({ collapseWhitespace: true })) 
-        .pipe(htmlpretty())
-        .pipe(gulp.dest(paths.html.dest))
-        .pipe(browserSync.stream())
-}
-
-function styles() {
-    return gulp.src(paths.styles.src)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefix({
-            cascade: false
-        }))
-        .pipe(gulp.dest(paths.styles.dest))
-        .pipe(cleanCSS({
-            level: 2
-        }))
-        .pipe(concat('style.min.css'))
-        .pipe(gulp.dest(paths.styles.dest))
-        .pipe(browserSync.stream())
-}
-
-function scripts() {
-    return gulp.src(paths.scripts.src)
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest(paths.scripts.dest))
-        .pipe(concat('main.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.scripts.dest))
-        .pipe(browserSync.stream())
-}
-
-function img() {
-    return gulp.src(paths.images.src)
-        .pipe(newer(paths.images.dest))
-        .pipe(webp())
-        .pipe(gulp.dest(paths.images.dest))
-        .pipe(gulp.src(paths.images.src))
-        .pipe(newer(paths.images.dest))
-        .pipe(imagemin())
-        .pipe(gulp.dest(paths.images.dest))
-        .pipe(browserSync.stream())
-}
-
-function tottf() {
-    return gulp.src(`${paths.fonts.src}*.otf`)
-        .pipe(fonter({
-            formats: ['ttf']
-        }))
-        .pipe(gulp.dest(paths.fonts.src))
-}
-
-function towoff() {
-    return gulp.src(`${paths.fonts.src}*.ttf`)
-        .pipe(newer(paths.fonts.dest))
-        .pipe(woff())
-        .pipe(gulp.dest(paths.fonts.dest))
-}
-
-function towoff2() {
-    return gulp.src(`${paths.fonts.src}*.ttf`)
-        .pipe(newer(paths.fonts.dest))
-        .pipe(woff2())
-        .pipe(gulp.dest(paths.fonts.dest))
-}
-
+import { clear } from "./config/tasks/clear.js";
+import { html } from "./config/tasks/html.js";
+import { styles } from "./config/tasks/styles.js"
+import { img } from "./config/tasks/img.js";
+import { scripts } from "./config/tasks/scripts.js";
+import { localhost } from "./config/tasks/localhost.js";
+import { tottf, towoff2 } from "./config/tasks/fonts.js";
+import { togit } from "./config/tasks/git.js";
 
 function watch() {
-    browserSync.init({
-        server: {
-            baseDir: "./dist/"
-        }
-    })
-    gulp.watch(paths.html.dest).on('change', browserSync.reload)
     gulp.watch(paths.styles.stylewatch, styles)
     gulp.watch(paths.html.htmlwatch, html)
     gulp.watch(paths.scripts.src, scripts)
     gulp.watch(paths.images.src, img)
     gulp.watch(`${paths.fonts.src}*.otf`, tottf)
-    gulp.watch(`${paths.fonts.src}*.ttf`, towoff)
     gulp.watch(`${paths.fonts.src}*.ttf`, towoff2)
 }
 
-function togit() {
-    return gulp.src('src/', 'gulpfile.js', 'package.json', 'README.md', 'gitingnore')
-        .pipe(git.add())
-        .pipe(git.commit('initial commit'))
-        .on('end', function() {
-            git.push('origin', 'master')
-        })
-}
+const build = gulp.series(clear, html, styles, scripts, tottf, towoff2, img, gulp.parallel(watch, localhost));
+const git = gulp.series(togit);
 
-function zipDist() {
-    return gulp.src('dist/**/*.*')
-        .pipe(zip('archive.zip'))
-        .pipe(gulp.dest('./'))
-}
+export {build};
+export {git};
 
-const build = gulp.series(clean, html, gulp.parallel(styles, scripts, img), tottf, towoff, towoff2, watch)
+// function zipDist() {
+//     return gulp.src('dist/**/*.*')
+//         .pipe(zip('archive.zip'))
+//         .pipe(gulp.dest('./'))
+// }
 
-exports.zipDist = zipDist;
-exports.clean = clean;
-exports.styles = styles;
-exports.scripts = scripts;
-exports.html = html;
-exports.img = img;
-exports.tottf = tottf;
-exports.towoff2 = towoff2;
-exports.towoff = towoff;
-exports.watch = watch;
-exports.togit = togit;
-exports.build = build;
-exports.default = build;
+
+
+// exports.zipDist = zipDist;
+// exports.clean = clean;
+// exports.styles = styles;
+// exports.scripts = scripts;
+// exports.html = html;
+// exports.img = img;
+// exports.tottf = tottf;
+// exports.towoff2 = towoff2;
+// exports.towoff = towoff;
+// exports.watch = watch;
+// exports.togit = togit;
+// exports.build = build;
+// exports.default = build;
